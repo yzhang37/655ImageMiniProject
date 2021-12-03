d30cd14a4a3ef7aebf07601b6d9b1d6a417d72c4
青椒 <johnyin@bu.edu>
2021-12-03 03:56:46 -0500

    添加部分manager处理代码

--- manager/main.py
+++ manager/main.py
@@ -1,20 +1,35 @@
+import uuid
 import time
-from flask import Flask
+from flask import Flask, request
+from base58 import b58encode, b58decode
+import json
+FLICKR_ALPHABET = \
+    b'123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/task', methods=["POST", "OPTIONS"])
+def handle_picture():
+    if request.method == 'POST':
+        # 生成一个任务 id，供前端使用
+        uuid_byte = uuid.uuid4().bytes
+        short_uuid = b58encode(uuid_byte, FLICKR_ALPHABET)
 
+        # 读取 forms 中的 file 数据，并进行存储
+        # TODO: 
 
-@app.route("/index")
-def index_test():
-    print("index test,sleep for 15 second")
-    time.sleep(15)
-    return "index test for non-block"
+        # 最后，返回任务 id 给前端
+        return json.dumps({"task_id": short_uuid.decode()}, separators=(',', ':'))
+    elif request.method == 'OPTIONS':
+        return """Usage:
+  Upload a new picture (to submit an image recognition task). Manager will return a task id from the front end.
+- METHOD: POST
+- Parameter Format: Form data/FILE
+- Return Format: JSON
+- Return Data: { "task_id": <task_id, type: string, format: base58 of an random generated uuid> }
+"""
 
 
 if __name__ == '__main__':
