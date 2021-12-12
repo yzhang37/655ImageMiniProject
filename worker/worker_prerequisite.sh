bdf76017870b419f5b0989745fdcd1ec7f09ca43
Yuhang Sun <39935123+sunyuhang01@users.noreply.github.com>
2021-12-12 22:45:24 +0800

    Update worker_prerequisite.sh

--- worker/worker_prerequisite.sh
+++ worker/worker_prerequisite.sh
@@ -2,6 +2,6 @@
 wget https://raw.githubusercontent.com/yzhang37/655ImageMiniProject/main/worker/imagenet_label.json
 wget https://raw.githubusercontent.com/yzhang37/655ImageMiniProject/main/worker/worker.py
 sudo apt-get update
-sudo apt-get install python3-pip
-sudo apt-get install libjpeg-dev zlib1g-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y libjpeg-dev zlib1g-dev
 pip3 --no-cache-dir install torch==1.10.0+cpu torchvision==0.11.1+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
