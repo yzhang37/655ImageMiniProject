c5b9e1b4b4be9cc6894d26b6181d5c24ca4cf4e7
青椒 <10142130151_ecnu@outlook.com>
2021-12-12 01:01:53 -0500

    edit files

--- run_server.sh
+++ run_server.sh
@@ -1,10 +1,3 @@
 work_path=$(dirname $(readlink -f $0))
-cd ${work_path}/manager
-pip install -r requirements.txt
-ret=$?
-if [ $ret -ne 0 ]; then
-  echo -e "\e[31mFailed to install required python requirements.\e[0m"
-  exit
-fi
-clear
+cd ${work_path}
 python -m main --hostname 0.0.0.0 --port 80
